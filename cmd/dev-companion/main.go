b31e14c257f95e4173423e5995d8879c34669e19
Marco Widesott <marco.widesott@anitia.it>
2025-12-05 17:33:54 +0100

    server online!

--- cmd/dev-companion/main.go
+++ cmd/dev-companion/main.go
@@ -1,5 +1,46 @@
-package devcompanion
+package main
+
+import (
+	"dev-companion/internal/config"
+	"dev-companion/internal/core/projects"
+	"dev-companion/internal/http/handlers"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
+	var path = "config/config.json"
+
+	val, err := config.LoadConfig(path)
+	if err != nil {
+		fmt.Println("Errore durante il caricamento della configurazione: ", err)
+		os.Exit(1)
+	}
+
+	dir, err1 := projects.NewProjectRepository(val.DataDir)
+	if err1 != nil {
+		fmt.Println("Errore durante il caricamento del repository: ", err1)
+		os.Exit(1)
+	}
+
+	pjService := projects.CreatePjService(dir)
+
+	pjHandler := handlers.NewProjectHandler(pjService)
+
+	router := mux.NewRouter()
+	pjHandler.RegisterRoutes(router)
+
+	str := val.ListenAddress + ":" + strconv.Itoa(val.Port)
+	fmt.Println("Server Dev Companion in ascolto su: " + str)
+
+	err2 := http.ListenAndServe(str, router)
+	if err2 != nil {
+		fmt.Println("Errore durante l'avvio del server: ", err2)
+		os.Exit(-1)
+	}
 
 }
