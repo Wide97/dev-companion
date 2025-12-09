c829f8b521a29a56c5763c38efb0d4d2546af3ed
Marco Widesott <marco.widesott@anitia.it>
2025-12-09 17:20:24 +0100

    finished logging

--- internal/http/middleware/logging.go
+++ internal/http/middleware/logging.go
@@ -1,31 +1,54 @@
 package middleware
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 type LoggingResponseWriter struct {
 	ResponseWriter http.ResponseWriter
 	StatusCode     int
 }
 
+func (lw *LoggingResponseWriter) Header() http.Header {
+	return lw.ResponseWriter.Header()
+}
+
 func (lw *LoggingResponseWriter) WriteHeader(status int) {
 	lw.StatusCode = status
 	lw.ResponseWriter.WriteHeader(status)
-
 }
 
 func (lw *LoggingResponseWriter) Write(bytes []byte) (int, error) {
 	if lw.StatusCode == 0 {
-		lw.StatusCode = 200
+		lw.StatusCode = http.StatusOK
 	}
-	n, err := lw.ResponseWriter.Write(bytes)
-	if err != nil {
-		return n, err
-	}
-
-	return n, nil
-
+	return lw.ResponseWriter.Write(bytes)
 }
 
 func NewLoggingMiddleware() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
 
+			lw := &LoggingResponseWriter{
+				ResponseWriter: w,
+				StatusCode:     http.StatusOK,
+			}
+
+			next.ServeHTTP(lw, r)
+
+			duration := time.Since(start)
+
+			fmt.Printf(
+				"[%s] %s %s %d %s\n",
+				duration,
+				r.Method,
+				r.URL.Path,
+				lw.StatusCode,
+				r.RemoteAddr,
+			)
+		})
+	}
 }
