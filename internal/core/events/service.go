6277fe853b3ba2d9e6ea8b2c6b8a97dca50cb3b5
Marco Widesott <marco.widesott@anitia.it>
2025-12-10 16:16:38 +0100

    finish event service

--- internal/core/events/service.go
+++ internal/core/events/service.go
@@ -1,1 +1,124 @@
 package events
+
+import (
+	"strings"
+	"time"
+)
+
+type DomainError struct {
+	Code    string
+	Message string
+	Details map[string]string
+}
+
+type Service struct {
+	Repo *EventRepository
+}
+
+const ERROR_VALIDATION = "VALIDATION"
+const ERROR_NOT_FOUND = "NOT_FOUND"
+const ERROR_CONFLICT = "CONFLICT"
+const ERROR_INTERNAL = "INTERNAL"
+
+type EventFilter struct {
+	ProjectId string
+	Type      string
+	From      *time.Time
+	To        *time.Time
+}
+
+func (s *Service) ListEvents(filter EventFilter) ([]EventModel, error) {
+	errors := map[string]string{}
+	if filter.Type != "" && filter.Type != EventPjBuildFailed && filter.Type != EventPjBuildStarted && filter.Type != EventPjBuildSuccess && filter.Type != EventRunFailed && filter.Type != EventRunStarted && filter.Type != EventRunSuccess {
+		errors["type"] = "Tipo di event non valido"
+	}
+	if filter.From != nil && filter.To != nil && filter.From.After(*filter.To) {
+		errors["dateRange"] = "La data 'from' non può essere successiva alla data 'to'"
+	}
+	if len(errors) > 0 {
+		return []EventModel{}, NewValidationError(errors)
+	}
+	events, err := s.Repo.GetAll()
+	if err != nil {
+		return nil, NewInternalError("Errore nel recupero dell' event dal repository: " + err.Error())
+	}
+	filteredEvents := []EventModel{}
+	for _, event := range events {
+		if filter.ProjectId != "" && event.ProjectId != filter.ProjectId {
+			continue
+		}
+		if filter.Type != "" && event.Type != filter.Type {
+			continue
+		}
+		if filter.From != nil && event.CreatedAt.Before(*filter.From) {
+			continue
+		}
+		if filter.To != nil && event.CreatedAt.After(*filter.To) {
+			continue
+		}
+		filteredEvents = append(filteredEvents, event)
+	}
+
+	return filteredEvents, nil
+}
+
+func (s *Service) GetEvent(id string) (EventModel, error) {
+	if id == "" {
+		return EventModel{}, NewValidationError(map[string]string{"id": "id dell' event è obbligatorio"})
+	}
+	rm, err := s.Repo.GetById(id)
+	if err != nil {
+		if strings.Contains(err.Error(), "not found") {
+			return EventModel{}, NewNotFoundError(id)
+		}
+		return EventModel{}, NewInternalError("errore interno nel recupero dell' event: " + err.Error())
+	}
+
+	return rm, nil
+
+}
+
+func (s *Service) ListTodayEvents(now time.Time) ([]EventModel, error) {
+	startOfDay := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+	fil := EventFilter{
+		From:      &startOfDay,
+		To:        &now,
+		ProjectId: "",
+		Type:      "",
+	}
+	return s.ListEvents(fil)
+
+}
+
+func NewValidationError(details map[string]string) DomainError {
+	return DomainError{
+		Code:    ERROR_VALIDATION,
+		Message: "Errore di validazione",
+		Details: details,
+	}
+}
+func NewNotFoundError(id string) DomainError {
+	return DomainError{
+		Code:    ERROR_NOT_FOUND,
+		Message: "event con id " + id + " non trovato",
+		Details: map[string]string{"id": id},
+	}
+}
+
+func NewInternalError(message string) DomainError {
+	return DomainError{
+		Code:    ERROR_INTERNAL,
+		Message: message,
+		Details: nil,
+	}
+}
+
+func NewService(e *EventRepository) Service {
+	return Service{
+		Repo: e,
+	}
+}
+
+func (d DomainError) Error() string {
+	return d.Code + " Messaggio: " + d.Message
+}
---
+++ internal/http/handlers/events.go
@@ -0,0 +1,105 @@
+package handlers
+
+import (
+	"dev-companion/internal/core/events"
+	"dev-companion/internal/core/runs"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type EventsHandler struct {
+	service events.Service
+}
+
+func NewEventsHandler(svc events.Service) *EventsHandler {
+	newRunHan := EventsHandler{
+		service: svc,
+	}
+
+	return &newRunHan
+}
+
+func (h *RunsHandler) RegisterRunsRoutes(router *mux.Router) {
+
+	router.HandleFunc("/runs", func(w http.ResponseWriter, r *http.Request) {
+		h.ListRuns(w, r)
+	}).Methods("GET")
+
+	router.HandleFunc("/runs/{id}", func(w http.ResponseWriter, r *http.Request) {
+		h.GetRun(w, r)
+	}).Methods("GET")
+
+}
+
+func (h *RunsHandler) ListRuns(w http.ResponseWriter, r *http.Request) {
+	res, err := h.service.ListRuns(runs.RunFilter{})
+	if err != nil {
+		if domainErr, ok := err.(runs.DomainError); ok {
+			writeDomainRunError(w, domainErr)
+			return
+		}
+
+		internalErr := runs.NewInternalError(
+			"errore interno durante ListRuns: " + err.Error(),
+		)
+		writeDomainRunError(w, internalErr)
+		return
+	}
+
+	writeJson(w, http.StatusOK, res)
+}
+
+func (h *RunsHandler) GetRun(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	project, err := h.service.GetRun(id)
+	if err != nil {
+		if domainErr, ok := err.(runs.DomainError); ok {
+			writeDomainRunError(w, domainErr)
+			return
+		}
+
+		internalErr := runs.NewInternalError(
+			"errore interno durante GetRun: " + err.Error(),
+		)
+		writeDomainRunError(w, internalErr)
+		return
+
+	}
+	writeJson(w, 200, project)
+
+}
+
+func writeDomainEventError(w http.ResponseWriter, err runs.DomainError) {
+	code := err.Code
+
+	statusHttp := 500
+
+	switch {
+	case code == "VALIDATION":
+		statusHttp = 400
+	case code == "NOT_FOUND":
+		statusHttp = 404
+	case code == "CONFLICT":
+		statusHttp = 409
+	case code == "INTERNAL":
+		statusHttp = 500
+	default:
+		statusHttp = 500
+	}
+
+	type response struct {
+		Code    string
+		Message string
+		Details map[string]string
+	}
+
+	var body response
+	body.Code = err.Code
+	body.Message = err.Message
+	body.Details = err.Details
+
+	writeJson(w, statusHttp, body)
+
+}
